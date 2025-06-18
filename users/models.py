1417826c208a18a0e709491600a34d11437dadb3
jp066 <jp066>
2025-06-18 11:00:34 -0300

    criação do modelo usuario

--- users/models.py
+++ users/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class PerfilUsuario(models.TextChoices):
+    PAROCO = 'PAROCO', 'Pároco'
+    CATEQUISTA = 'CATEQUISTA', 'Catequista'
+    COORDENADOR_MINISTERIO = 'COORD_MINISTERIO', 'Coordenador de Ministério'
+    PASCOM = 'PASCOM', 'PASCOM'
+    FIEL = 'FIEL', 'Fiel da Comunidade'
+
+
+class Usuario(models.Model):
+    codigo_acesso = models.CharField(
+        max_length=100,
+        primary_key=True,
+        verbose_name='Código de Acesso'
+    )
+    comunidade = models.CharField(max_length=100, verbose_name='Comunidade')
+    perfil_usuario = models.CharField(
+        max_length=20,
+        choices=PerfilUsuario.choices,
+        default=PerfilUsuario.FIEL,
+        verbose_name="Perfil do Usuário"
+    )
+
+    identificador_paroco = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name='Identificador do Pároco'
+    )
